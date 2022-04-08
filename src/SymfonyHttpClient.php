f22eb2dc42bb2e39e99a407d133ccdb25c7ec6d2
charescape <charescape@outlook.com>
2022-04-08 18:25:49 +0800

    Update SymfonyHttpClient.php

--- src/SymfonyHttpClient.php
+++ src/SymfonyHttpClient.php
@@ -14,7 +14,7 @@ use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 final class SymfonyHttpClient
 {
     public static string $requestError;
-    public static string $requestDebug;
+    public static string $requestErrorDebug;
 
     public static function httpGet(string $url, array $options = [], bool $toArray = true)
     {
@@ -51,14 +51,14 @@ final class SymfonyHttpClient
             $statusCode = $resp->getStatusCode();
         } catch (TransportExceptionInterface $e) {
             self::$requestError = 'Network Error';
-            self::$requestDebug = $resp->getInfo('debug');
+            self::$requestErrorDebug = $resp->getInfo('debug');
             return null;
         }
 
         if ($throw) {
             if ($statusCode < 200 || $statusCode >= 300) {
                 self::$requestError = 'Status Code Error';
-                self::$requestDebug = $resp->getInfo('debug');
+                self::$requestErrorDebug = $resp->getInfo('debug');
                 return null;
             }
         }
@@ -67,23 +67,23 @@ final class SymfonyHttpClient
             return $toArray ? $resp->toArray($throw) : $resp->getContent($throw);
         } catch (ClientExceptionInterface $e) {
             self::$requestError = 'Invalid Request';
-            self::$requestDebug = $resp->getInfo('debug');
+            self::$requestErrorDebug = $resp->getInfo('debug');
             return null;
         } catch (DecodingExceptionInterface $e) {
             self::$requestError = 'Invalid Response';
-            self::$requestDebug = $resp->getInfo('debug');
+            self::$requestErrorDebug = $resp->getInfo('debug');
             return null;
         } catch (RedirectionExceptionInterface $e) {
             self::$requestError = 'Too Many Redirects';
-            self::$requestDebug = $resp->getInfo('debug');
+            self::$requestErrorDebug = $resp->getInfo('debug');
             return null;
         } catch (ServerExceptionInterface $e) {
             self::$requestError = 'Remote Service Error';
-            self::$requestDebug = $resp->getInfo('debug');
+            self::$requestErrorDebug = $resp->getInfo('debug');
             return null;
         } catch (TransportExceptionInterface $e) {
             self::$requestError = 'Network Error';
-            self::$requestDebug = $resp->getInfo('debug');
+            self::$requestErrorDebug = $resp->getInfo('debug');
             return null;
         }
     }

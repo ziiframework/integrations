9f9181ba4f1765163ec216e5e80410bbae065463
charescape <charescape@outlook.com>
2022-05-08 13:38:55 +0800

    Create ClassObject.php

---
+++ src/ClassObject.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Zii\Integrations;
+
+use yii\base\BaseObject;
+
+class ClassObject extends BaseObject
+{
+//    protected ?string $_runtimeError = null;
+//
+//    public function hasRuntimeError(): bool
+//    {
+//        return $this->_runtimeError !== null;
+//    }
+//
+//    public function getRuntimeError(): ?string
+//    {
+//        return $this->_runtimeError;
+//    }
+}

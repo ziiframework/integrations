78947f80072be1ad0f4ce562de82c7ac08f7a3ed
charescape <charescape@outlook.com>
2023-03-31 21:18:16 +0800

    Create MonoLogger.php

---
+++ src/MonoLogger.php
@@ -0,0 +1,87 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Zii\Integrations;
+
+use DateTimeZone;
+use Doctrine\Inflector\Language;
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
+use Yii;
+use Doctrine\Inflector\InflectorFactory;
+
+abstract class MonoLogger
+{
+    private string $_dir;
+    private string $_category;
+    private string $_sessionId;
+
+    public function __construct(string $dir, string $category, string $sessionId)
+    {
+        $this->_dir = $dir;
+        $this->_category = $category;
+        $this->_sessionId = $sessionId;
+    }
+
+    private function logger(): Logger
+    {
+        $inflector = InflectorFactory::createForLanguage(Language::ENGLISH)->build();
+
+        $uname = pf_posix_username('nobody');
+        $filename = $inflector->urlize($this->_category);
+        $date = date('Ym');
+
+        return new Logger(
+            $this->_category,
+            [
+                new StreamHandler($this->_dir . sprintf('/debug.%s.%s.%s.log',   $date, $uname, $filename), Logger::DEBUG, false),
+                new StreamHandler($this->_dir . sprintf('/info.%s.%s.%s.log',    $date, $uname, $filename), Logger::INFO, false),
+                new StreamHandler($this->_dir . sprintf('/warning.%s.%s.%s.log', $date, $uname, $filename), Logger::WARNING, false),
+                new StreamHandler($this->_dir . sprintf('/error.%s.%s.%s.log',   $date, $uname, $filename), Logger::ERROR, true),
+            ],
+            [],
+            new DateTimeZone(Yii::$app->timeZone)
+        );
+    }
+
+    /**
+     * @param string|int|float $message
+     * @param mixed|null $context
+     * @return void
+     */
+    public function debug($message, $context = null): void
+    {
+        $this->logger()->debug('[' . $message . ']', ['sessionId' => $this->_sessionId, 'context' => $context]);
+    }
+
+    /**
+     * @param string|int|float $message
+     * @param mixed|null $context
+     * @return void
+     */
+    public function info($message, $context = null): void
+    {
+        $this->logger()->info('[' . $message . ']', ['sessionId' => $this->_sessionId, 'context' => $context]);
+    }
+
+    /**
+     * @param string|int|float $message
+     * @param mixed|null $context
+     * @return void
+     */
+    public function warning($message, $context = null): void
+    {
+        $this->logger()->warning('[' . $message . ']', ['sessionId' => $this->_sessionId, 'context' => $context]);
+    }
+
+    /**
+     * @param string|int|float $message
+     * @param mixed|null $context
+     * @return void
+     */
+    public function error($message, $context = null): void
+    {
+        $this->logger()->error('[' . $message . ']', ['sessionId' => $this->_sessionId, 'context' => $context]);
+    }
+}

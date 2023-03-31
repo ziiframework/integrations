004411a09afc93da7381a2a80818d070d0740a98
charescape <charescape@outlook.com>
2023-03-31 21:58:25 +0800

    Update MonoLoggerTest.php

--- tests/MonoLoggerTest.php
+++ tests/MonoLoggerTest.php
@@ -18,17 +18,22 @@ class MonoLoggerTest extends TestCase
         $logger = new MonoLogger(RUNTIME_DIR, 'UnitTest', 'test-session-id');
 
         $logger->debug('test debug message');
+        clearstatcache();
         dump('after debug:', scandir(RUNTIME_DIR));
 
         $logger->info('test info message');
+        clearstatcache();
         dump('after info:', scandir(RUNTIME_DIR));
 
         $logger->warning('test warning message');
+        clearstatcache();
         dump('after warning:', scandir(RUNTIME_DIR));
 
         $logger->error('test error message');
+        clearstatcache();
         dump('after error:', scandir(RUNTIME_DIR));
 
-        $this->assertFileExists(count(scandir(RUNTIME_DIR)) === 7);
+        clearstatcache();
+        $this->assertTrue(count(scandir(RUNTIME_DIR)) === 7);
     }
 }

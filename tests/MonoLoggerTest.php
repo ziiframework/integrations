79a93984c51351072a553e6f1ee82c24c6a5ba4f
charescape <charescape@outlook.com>
2023-04-02 11:37:36 +0800

    Update MonoLoggerTest.php

--- tests/MonoLoggerTest.php
+++ tests/MonoLoggerTest.php
@@ -18,21 +18,21 @@ class MonoLoggerTest extends TestCase
     {
         $dir = RUNTIME_DIR . '/' . time() . '_' . random_int(111_111, 999_999);
 
-        $logger = new MonoLogger($dir, 'UnitTest', 'test-session-id');
-
         $uname = pf_posix_username('nobody');
         $date = date('Ym');
 
         foreach (['debug', 'info', 'warning', 'error'] as $idx => $level) {
+            $logger = new MonoLogger($dir, 'UnitTest', 'test-session-id');
+
+            // default without GlobalVars
             $logger->$level("test $level message");
             $this->assertSame(2 + $idx + 1, count(scandir($dir)));
 
             $log_file = $dir . "/$level.$date.$uname.unit-test.log";
 
-            $logger->close();// flush first
+            $logger->close();// flush
             $log_file_contents = file_get_contents($log_file);
 
-            // default without GlobalVars
             $this->assertStringContainsString(
                 sprintf('UnitTest.%s: [test %s message] {', strtoupper($level), $level),
                 $log_file_contents
@@ -44,7 +44,7 @@ class MonoLoggerTest extends TestCase
             // set with GlobalVars
             $logger->withGlobalVars(true);
             $logger->$level("test $level message");
-            $logger->close();// flush first
+            $logger->close();// flush
             $log_file_contents = file_get_contents($log_file);
             $this->assertStringContainsString('"__SERVER":{', $log_file_contents);
         }
